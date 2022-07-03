284d5b439373229f7bc9243d275b10e826f95dee
ngocxxu <ngocquach43@gmail.com>
2022-07-03 12:53:35 +0700

    complete set up origin project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './App.css';
+import './App.scss';
 import { ErrorTemplate } from "./pages/ErrorTemplate";
 import {  HomeTemplate } from "./pages/HomeTemplate";
 import { Home } from "./pages/HomeTemplate/Home";
